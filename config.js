d7951db347ec07a8387a8a2d9581524a15d45018
Akshaya Raghuvanshi <scaledrop@gmail.com>
2018-05-02 12:56:06 +0530

    update config details

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.appPort = 1300;
 exports.serverTimeout = 20000;
 exports.database = {
-	path: 'mongodb://192.168.39.82:27017/', // live server
-	name: 'eterrorlog',
+	path: 'mongodb://localhost:27017/', // live server
+	name: 'errorlog',
 	collection: 'urls'
 }
